3fc04c5e6c726e06c728af0650ceed5a66261880
dmytra bob <105235692+dmytra@users.noreply.github.com>
2024-11-11 16:19:07 +0200

    std--vector<thread> threads(n).cpp
    https://stackoverflow.com/questions/266168/simple-example-of-threading-in-c

---
+++ threads/std--vector<thread> threads(n).cpp
@@ -0,0 +1,29 @@
+#include <thread>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void doSomething(int id) {
+    cout << id << "\n";
+}
+
+/**
+ * Spawns n threads
+ */
+void spawnThreads(int n)
+{
+    std::vector<thread> threads(n);
+    // spawn n threads:
+    for (int i = 0; i < n; i++) {
+        threads[i] = thread(doSomething, i + 1);
+    }
+
+    for (auto& th : threads) {
+        th.join();
+    }
+}
+
+int main()
+{
+    spawnThreads(10);
+}
